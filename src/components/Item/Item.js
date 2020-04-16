9b8d2938490dc924d7148c00fdd07e1744838e94
Armando Velazquez <davidarmando.gdl@live.com>
2020-04-16 02:24:42 -0500

    [fix] styles

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -7,12 +7,8 @@ const item = (props) => {
   return (
     <article className="listItem__container">
       <span style={{ fontWeight: "bold" }}>{props.itemId}</span>
-      <span style={{ color: "blue", fontWeight: "bold" }}>
-        {props.itemName}
-      </span>
-      <span style={{ color: "green", fontWeight: "bold" }}>
-        ${props.amount}.00
-      </span>
+      <span className="listItem__name">{props.itemName}</span>
+      <span className="listItem__amount">${props.amount}.00</span>
       <span className="listItem__date">{props.date}</span>
       <span>{props.category}</span>
       <span>
